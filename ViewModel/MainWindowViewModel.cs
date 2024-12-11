5624c72d1ff0e5288ecf4c3d3693a03597bb6b85
piglet <2792904471@qq.com>
2024-12-12 00:45:24 +0800

    MainWindow-UI

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Linq;
 
 namespace ImageProcess.ViewModel;
 
@@ -15,17 +16,29 @@ public partial class MainWindowViewModel : ObservableObject
     private ImageFile? selectedFile;
 
     [ObservableProperty]
-    private ObservableCollection<string> processTypes = new()
-    {
-        "灰度",
-        "放大200%",
-        "缩小50%",
-        "顺时针旋转90°",
-        "逆时针旋转90°"
-    };
+    private ObservableCollection<string> processTypes;
 
     [ObservableProperty]
     private string? selectedProcessType;
+
+    public MainWindowViewModel()
+    {
+        // 初始化处理类型列表
+        ProcessTypes = new ObservableCollection<string>
+        {
+            "灰度",
+            "放大至200%",
+            "缩小至50%",
+            "顺时针旋转90°",
+            "逆时针旋转90°",
+            "边缘检测",
+            "二值化",
+            "模糊"
+        };
+        
+        // 设置默认选中第一项
+        SelectedProcessType = ProcessTypes.FirstOrDefault();
+    }
 
     [RelayCommand]
     private void AddImages()
